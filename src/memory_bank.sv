88486079a9e46a5497c9ecb02978c3bb7e7dc0f7
justinscottlee <justin.s.lee.work@gmail.com>
2023-01-29 18:19:52 -0800

    added memory_bank module

---
+++ src/memory_bank.sv
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+module memory_bank #(
+    parameter DATA_DEPTH = 4096
+    )(
+    input logic clk, we,
+    input logic [$clog2(DATA_DEPTH)-1:0] addr,
+    input byte write_data,
+    output byte read_data
+    );
+    
+    byte mem [0:DATA_DEPTH-1];
+
+    always_ff @ (posedge clk) begin
+        if (we) begin
+            mem[addr] <= write_data;
+        end
+        else begin
+            read_data <= mem[addr];
+        end
+    end
+endmodule
