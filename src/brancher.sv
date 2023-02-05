5207262c7d451affce2d389d2cbe2e08f63b1df3
justinscottlee <justin.s.lee.work@gmail.com>
2023-02-05 10:44:02 -0800

    removed clocking and extraneous code from brancher

--- src/brancher.sv
+++ src/brancher.sv
@@ -3,7 +3,6 @@
 `include "defines.vh"
 
 module brancher(
-    input logic clk,
     input logic [1:0] branch_condition,
     input integer alu_result,
     output logic branch_taken,
@@ -14,16 +13,12 @@ module brancher(
     
     assign branch_addr = pc + immediate;
     
-    always_ff @ (posedge clk) begin
+    always_comb begin
         case (branch_condition)
-        `BRANCH_ALU_ZERO:       branch_taken <= alu_result == 0;
-        `BRANCH_ALU_NONZERO:    branch_taken <= alu_result != 0;
-        `BRANCH_FORCE_FALSE:    branch_taken <= 1'b0;
-        `BRANCH_FORCE_TRUE:     branch_taken <= 1'b1;
+        `BRANCH_ALU_ZERO:       branch_taken = alu_result == 0;
+        `BRANCH_ALU_NONZERO:    branch_taken = alu_result != 0;
+        `BRANCH_FORCE_FALSE:    branch_taken = 1'b0;
+        `BRANCH_FORCE_TRUE:     branch_taken = 1'b1;
         endcase
-    end
-    
-    always_ff @ (posedge clk) begin
-        branch_addr <= pc + immediate;
     end
 endmodule
